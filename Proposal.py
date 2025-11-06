c300142d32817424e209fbad591c368f09919a6b
chloeyuan-a <yyuan6073@dbu.edu>
2025-11-05 18:35:43 -0600

    Update Proposal.py

--- Proposal.py
+++ Proposal.py
@@ -1,7 +1,10 @@
 """
-Project Proposal:
+Project Proposal: Fake vs Real News Classifier
 
 Project Overview:
+The project is aim to develop a text classification model using natural language processing (NLP) to distinguish between fake and real news articles.
+This model will be trained on a labeled dataset containing real and fake news articles.
+The textual data will be transformed into numerical features suitable for machine learning algorithms.
 
 Project Objectives:
 
