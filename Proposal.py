c5c568d61e2343fe6e1563af4d139c155e033ed5
chloeyuan-a <yyuan6073@dbu.edu>
2025-11-05 18:27:19 -0600

    Create Proposal.py

---
+++ Proposal.py
@@ -0,0 +1,28 @@
+"""
+Project Proposal:
+
+Project Overview:
+
+Project Objectives:
+
+
+Scope of Work:
+
+Phase 1:
+Phase 2:
+Phase 3:
+Phase 4:
+Phase 5:
+Phase 6:
+
+
+Deliverables:
+
+Timeline:
+
+
+Risks and Mitigation:
+
+
+Conclusion:
+"""
