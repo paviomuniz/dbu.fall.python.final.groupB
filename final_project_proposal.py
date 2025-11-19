14421b1f6028a45f3cb9616f58e64980c3b5419e
chloeyuan-a <yyuan6073@dbu.edu>
2025-11-19 00:39:48 -0600

    Update final_project_proposal.py
    Update final project proposal

--- final_project_proposal.py
+++ final_project_proposal.py
@@ -1,44 +1,54 @@
 
 """
-Project Proposal: Analyzing News Sentiment vs. Google Stock Price Movements
-1. Project Title
-Predicting Stock Price Movements Using News Sentiment Analysis (Google Stock Dataset)
+Project Proposal: Stock price movement prediction based on financial news sentiment
 
-2. The purpose of this project is to analyze whether news sentiment has a measurable impact on Google’s daily stock price movements. Using historical stock price data and financial news articles, we will compute sentiment scores and examine how they correlate with real market behavior.
+Project Overview:
+This project aim to predict stock price movements using financial news sentiment analysis.
+The purpose of this project is to analyze whether news sentiment has a measurable impact on Google’s stock price movements. 
+Using historical stock price data and financial news articles. We will compute sentiment scores and examine how they correlate with real market behavior.
+We will develop a simple machine learning model that predicts stock price direction (up or down) based on sentiment indicators, 
+and deploy the results in an interactive web application so users can interactively view sentiment and stock price predictions.
 
-Additionally, we will experiment with a simple machine learning model that predicts short-term stock price direction (up or down) based on sentiment indicators. Finally, we plan to deploy the results in a Flask web application so users can interactively view sentiment, charts, and model predictions.
+Project Objectives:
+1. **Data Collection and Preprocessing**:
+Dataset: Google Daily Stock Prices (2004–Today) https://www.kaggle.com/datasets/emrekaany/google-daily-stock-prices-2004-today
+         Google Financial News Dataset (optional / based on availability) https://www.kaggle.com/datasets/emrekaany/google-googl-financial-news-from-2000-to-today/
+Extra data: NewsAPI, public stock price API
 
-3. Datasets to Be Used
+2. **Model Development**:
+Develop sentiment analysis model and price predictive model
 
-1.	Google Daily Stock Prices (2004–Today)
-                              
-https://www.kaggle.com/datasets/emrekaany/google-daily-stock-prices-2004-today
+3. **Model Evaluation and Optimization**:
+Evaluate model performance using appropriate metrics and optimize the model
 
-2.	Google Financial News Dataset (optional / based on availability)
-https://www.kaggle.com/datasets/emrekaany/google-googl-financial-news-from-2000-to-today/
+4. **Visualization and Reporting**:
+Build appropriate visulization charts and write a summary report
 
-4. Scope of Work
-This is what we intend to accomplish:
-A. Data Collection & Cleaning
+5. **Interactive Web App       
+Building a streamlit app displaying historical stock price and stock price prediction
+List recent top news about google
+
+
+Scope of Work:
+**Phase 1: Requirements Gathering**
+- Dateset identification
+- Business goal identification
+- FE design
+
+**Phase 2: Data Collection and Preprocessing**
 •	Load stock price dataset (Open, Close, High, Low, Volume).
 •	Load financial news dataset (news title, date).
 •	Clean text data (remove punctuation, stopwords, special characters).
 •	Align news dates with corresponding stock trading days.
 
-B. Sentiment Analysis
+**Phase 3: Model Development**
+- Sentiment Analysis model
 •	Use a Python NLP library (VADER, TextBlob, or HuggingFace model).
 •	Generate sentiment scores for each news headline/article.
 •	Categorize sentiment as positive, neutral, or negative.
-
-C. Exploratory Data Analysis
-We will analyze:
-•	Does positive news correlate with price increases?
-•	Does negative news correlate with price drops?
-•	How strong is the correlation between sentiment score and daily returns?
-•	Plot sentiment vs. stock price over time.
-
-D. Predictive Modeling (Simple ML)
-We will build a basic model to predict price movement (Up/Down) using:
+- Analyze correlation between news sentiment score and stock price movement
+- Predictive Modeling (Simple ML)
+Convert sentiment into predictive features and build a model to predict price movement (Up/Down) using:
 •	Sentiment score
 •	Lagged stock returns
 •	Volume
@@ -49,39 +59,45 @@ Possible models:
 •	Support Vector Machine
 Goal: Predict whether the next day’s price will go up or down based on sentiment.
 
-E. Deliverables
-Our final project deliverables will include:
-•	A Python script/Visual Studio explaining each step.
-•	Visualizations (sentiment over time, correlation graphs, prediction accuracy).
-•   A Flask web application to display results.
-•	A short report summarizing the findings, challenges, and predictive results.
+**Phase 4: Model Evaluation and Optimization**
+- Using appropriate performance metrics (accuracy, precision, F1 score)
 
-F. Flask Web Application
+**Phase 5: Visualization and Reporting**
+- Build appropriate visulization charts using Matplotlib / Seaborn
+- Write a summary report
 
-We will develop a Flask web interface to present the project results in a simple dashboard.
+**Phase 6: Deployment: Interactive Web App**
+Develop an interactive web interface to present the project results in a simple dashboard and allow users to explore insights through a browser.
 The web app will display:
+•   Correlation chart: Sentiment scores vs stock price 
+•   Stock price + predictions 
+•   List top news and sentiment
 
-•   Sentiment scores
-•   Stock price charts
-•   Correlation visualizations
-•   Model predictions (Up/Down)
-•   Optionally: Upload or paste custom news headlines for sentiment evaluation
 
-This adds an interactive component and allows users to explore insights through a browser.
-
-5. Expected Outcome
-We expect to determine:
+Deliverables:
+•	A Python script includes data cleaning and preprocessing, model development, model evaluation and visualization.
+•   An interactive web application.
+•	A short report summarizing the findings, challenges, and predictive results.
+Expected Outcome
 •	Whether strong positive sentiment leads to upward price movement.
 •	Whether negative sentiment predicts a drop.
 •	How accurately a simple model can predict price direction based on sentiment.
 The project does not aim to build a real trading system—just to explore the relationship between news sentiment and stock performance using Python.
 
-6. Tools & Technologies
-•	Python
-•	Pandas, NumPy
-•	NLTK / VADER / TextBlob (for sentiment)
-•	Matplotlib / Seaborn
-•	Scikit-learn
+Tools & Technologies:
+•	Programming language: Python
+•	Libraries: Pandas, NumPy, NLTK / VADER / TextBlob (for sentiment), Matplotlib / Seaborn, Scikit-learn
 •	Flask (for Interactive web interface)
+•	Version Control: Github
 
+Timeline:
+Week_1: Phase 1 Requirements Gathering & Phase 2 Data Collection and Preprocessing
+Week_2: Phase 3 Model Development
+Week_3: Phase 4 Model Evaluation and Optimization & Phase 5 Visualization and Reporting
+Week_4: Phase 6 Interactive Web App
+
+Risk:
+- Stock prices are influenced by many other factors, and news sentiment alone shows a weak or inconsistent relationship with stock price movements. 
+ For example, markets react before news is published, news are not meaningful or fake news, news sentiment doesn't mean investor sentiment.
+     
 """
